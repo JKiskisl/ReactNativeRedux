8a38f1ba770578c45dd4b8839a529b7327c7cf14
JKiskisl <smicscl@gmail.com>
2022-11-23 18:57:13 +0200

    AlmostFinal

--- src/screens/deleteorupdate/DeleteorUpdate.js
+++ src/screens/deleteorupdate/DeleteorUpdate.js
@@ -5,21 +5,95 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from 'react-native'
 import { firebase } from '@react-native-firebase/database'
 import { useCurrentUser } from '../../Core/onboarding'
-import { useRoute } from '@react-navigation/native'
 
-function DeleteorUpdate({route}) {
+function DeleteorUpdate({ route }) {
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [price, setPrice] = useState('')
+  const [userID, setuserID] = useState('')
 
+  const currentUser = useCurrentUser()
 
-  const {postpass} = route.params
-  console.log(postpass)
+  const { postpass } = route.params
+
+  const reference = firebase
+    .app()
+    .database('https://fir-addtest-e6b1c-default-rtdb.firebaseio.com/')
+
+  handleSubmit = () => {
+    const newRef = reference.ref('posts/' + postpass.id)
+    newRef
+      .update({
+        id: postpass.id,
+        name: name,
+        description: description,
+        price: price,
+        userPosted: postpass.userPosted,
+      })
+      .then(() => console.log('Data updated'))
+      
+    setName('')
+    setDescription('')
+    setPrice('')
+    setuserID('')
+  }
+
+  const deleteItem = (id) => {
+    reference
+      .ref('posts/' + id)
+      .remove()
+      .then(() => console.log('success', id))
+      .catch(() => console.log('nope'))
+  }
+
+  const successDelete = () =>{
+    Alert.alert("Deleted succesfully")
+  }
 
   return (
-    <View>
-      <Text>{postpass.name} {postpass.id}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Add post</Text>
+      <View styles={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={name}
+          placeholder={postpass.name}
+          onChangeText={text => setName(text)}
+        />
+        <TextInput
+          style={styles.input}
+          value={description}
+          placeholder={postpass.description}
+          onChangeText={text => setDescription(text)}
+        />
+        <TextInput
+          style={styles.input}
+          value={price}
+          placeholder={postpass.price}
+          onChangeText={text => setPrice(text)}
+        />
+      </View>
+      <View style={styles.addButtonContainer}>
+        <TouchableOpacity onPress={handleSubmit}>
+          <View style={styles.addButton}>
+            <Text style={styles.addButtonText}>Update</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.addButtonContainer}>
+        <TouchableOpacity onPress={() => deleteItem(postpass.id)}
+        onPressIn={() => successDelete()}>
+          <View style={styles.deleteButton}>
+            <Text style={styles.addButtonText}>DELETE</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
+    
   )
 }
 
@@ -65,6 +139,15 @@ const styles = StyleSheet.create({
     flex: 4,
     justifyContent: 'flex-start',
     alignItems: 'center',
+  },
+  deleteButton: {
+    width: 120,
+    height: 40,
+    backgroundColor: 'red',
+    marginLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
   },
   title: {
     paddingTop: 30,
